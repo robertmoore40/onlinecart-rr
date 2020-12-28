aa09b962e8e788be3a8a7b88fada87f71621e479
robertmoore40 <robertcmoore92@gmail.com>
2020-12-28 17:51:34 -0500

    improted from zoom

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,3 +3,6 @@ import formatCurrency from "../util";
 
 import Fade from "react-reveal/Fade";
 import { connect } from "react-redux";
+
+import Modal from "react-modal";
+import Zoom from "react-reveal/Zoom";
