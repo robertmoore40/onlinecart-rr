fac67c9acfe6344466afc8189d37b76a7e0d03bc
robertmoore40 <robertcmoore92@gmail.com>
2020-12-29 14:23:45 -0500

    added cartridges

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -44,3 +44,7 @@ class Cart extends Component {
                 {cartItems.length === 0 ? (
           <div className="cart cart-header">Cart is empty</div>
         ) : (
+            <div className="cart cart-header">
+            You have {cartItems.length} in the cart{" "}
+          </div>
+        )}
