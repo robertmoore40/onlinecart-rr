2e4b3a4902c21358c82ae83eda9dfb76adc2d098
robertmoore40 <robertcmoore92@gmail.com>
2020-12-30 13:51:15 -0500

    added length

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -119,3 +119,7 @@ class Cart extends Component {
               </ul>
             </Fade>
           </div>
+          {cartItems.length !== 0 && (
+            <div>
+              <div className="cart">
+                <div className="total">
