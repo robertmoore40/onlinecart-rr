9a5cfc149259f72f826ba3d4bcfd3ccb3c4fe621
robertmoore40 <robertcmoore92@gmail.com>
2020-12-31 12:57:49 -0500

    add filter component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,3 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { filterProducts, sortProducts } from "../actions/productActions";
+
+class Filter extends Component {
+    render() {
+      return !this.props.filteredProducts ? (
