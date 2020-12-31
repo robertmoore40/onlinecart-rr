28945d67a7f3f30d8eb28b3ebe2fc1b18f4a6add
robertmoore40 <robertcmoore92@gmail.com>
2020-12-31 12:58:02 -0500

    added filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -5,3 +5,6 @@ import { filterProducts, sortProducts } from "../actions/productActions";
 class Filter extends Component {
     render() {
       return !this.props.filteredProducts ? (
+        <div>Loading...</div>
+        ) : (
+          <div className="filter">
