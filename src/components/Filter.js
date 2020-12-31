db120bf1bc74cfe27034b52d3e8b648f98367fdc
robertmoore40 <robertcmoore92@gmail.com>
2020-12-31 13:02:57 -0500

    added formnatting

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -6,48 +6,48 @@ class Filter extends Component {
     render() {
       return !this.props.filteredProducts ? (
         <div>Loading...</div>
-        ) : (
-          <div className="filter">
-              <div className="filter-result">
-          {this.props.filteredProducts.length} Products
-        </div>
-        <div className="filter-sort">
-          Order{" "}
-          <select
-            value={this.props.sort}
-            onChange={(e) =>
+      ) : (
+        <div className="filter">
+          <div className="filter-result">
+            {this.props.filteredProducts.length} Products
+          </div>
+          <div className="filter-sort">
+            Order{" "}
+            <select
+              value={this.props.sort}
+              onChange={(e) =>
                 this.props.sortProducts(
                   this.props.filteredProducts,
                   e.target.value
-                  )
-                }
-              >
-                    <option value="latest">Latest</option>
-            <option value="lowest">Lowest</option>
-            <option value="highest">Highest</option>
+                )
+              }
+            >
+              <option value="latest">Latest</option>
+              <option value="lowest">Lowest</option>
+              <option value="highest">Highest</option>
             </select>
-        </div>
-        <div className="filter-size">
-          Filter{" "}
-          <select
-            value={this.props.size}
-            onChange={(e) =>
-              this.props.filterProducts(this.props.products, e.target.value)
-            }
-          ></select>
-           <option value="">ALL</option>
-            <option value="XS">XS</option>
-            <option value="S">S</option>
-            <option value="M">M</option>
-            <option value="L">L</option>
-            <option value="XL">XL</option>
-            <option value="XXL">XXL</option>
-          </select>
           </div>
-      </div>
-    );
-}
-}
+          <div className="filter-size">
+            Filter{" "}
+            <select
+              value={this.props.size}
+              onChange={(e) =>
+                this.props.filterProducts(this.props.products, e.target.value)
+              }
+            >
+              <option value="">ALL</option>
+              <option value="XS">XS</option>
+              <option value="S">S</option>
+              <option value="M">M</option>
+              <option value="L">L</option>
+              <option value="XL">XL</option>
+              <option value="XXL">XXL</option>
+            </select>
+          </div>
+        </div>
+      );
+    }
+  }
 export default connect(
   (state) => ({
     size: state.products.size,
