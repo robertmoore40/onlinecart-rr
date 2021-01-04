ff5078788697e7e1de6fb25d91f436dbf878fca3
robertmoore40 <robertcmoore92@gmail.com>
2021-01-04 14:16:55 -0500

    added fetch and add cart

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,3 +6,6 @@ import Modal from "react-modal";
 
 import Zoom from "react-reveal/Zoom";
 import { connect } from "react-redux";
+
+import { fetchProducts } from "../actions/productActions";
+import { addToCart } from "../actions/cartActions";
