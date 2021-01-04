ed43a54dcd7b9d2dee9d6d187d094d8782859b4d
robertmoore40 <robertcmoore92@gmail.com>
2021-01-04 14:20:51 -0500

    added fade

--- src/components/Products.js
+++ src/components/Products.js
@@ -18,45 +18,45 @@ class Products extends Component {
       };
     }
     componentDidMount() {
-        this.props.fetchProducts();
-      }
-      openModal = (product) => {
-        this.setState({ product });
-      };
-      closeModal = () => {
-        this.setState({ product: null });
-      };
-      render() {
-        const { product } = this.state;
-        return (
-            <div>
-        <Fade bottom cascade>
-          {!this.props.products ? (
-            <div>Loading...</div>
-          ) : (
-            <ul className="products">
-            {this.props.products.map((product) => (
-              <li key={product._id}>
+      this.props.fetchProducts();
+    }
+    openModal = (product) => {
+      this.setState({ product });
+    };
+    closeModal = () => {
+      this.setState({ product: null });
+    };
+    render() {
+      const { product } = this.state;
+      return (
+        <div>
+          <Fade bottom cascade>
+            {!this.props.products ? (
+              <div>Loading...</div>
+            ) : (
+              <ul className="products">
+                {this.props.products.map((product) => (
+                  <li key={product._id}>
                     <div className="product">
-                    <a
-                      href={"#" + product._id}
-                      onClick={() => this.openModal(product)}
-                    ></a>
-                     <img src={product.image} alt={product.title}></img>
-                      <p>{product.title}</p>
-                    </a>
-                    <div className="product-price">
-                      <div>{formatCurrency(product.price)}</div>
-                      <button
-                       onClick={() => this.props.addToCart(product)}
-                       className="button primary"
-                     >
+                      <a
+                        href={"#" + product._id}
+                        onClick={() => this.openModal(product)}
+                      >
+                        <img src={product.image} alt={product.title}></img>
+                        <p>{product.title}</p>
+                      </a>
+                      <div className="product-price">
+                        <div>{formatCurrency(product.price)}</div>
+                        <button
+                          onClick={() => this.props.addToCart(product)}
+                          className="button primary"
+                        >
                           Add To Cart
-                      </button>
+                        </button>
+                      </div>
                     </div>
-                  </div>
-                    </li>
-                    ))}
-                  </ul>
-                )}
-              </Fade>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </Fade>
