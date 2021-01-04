9afec561bc94c0d0bb7bab421b9cfa80ced5ccd5
robertmoore40 <robertcmoore92@gmail.com>
2021-01-04 14:20:23 -0500

    added div buttons

--- src/components/Products.js
+++ src/components/Products.js
@@ -51,3 +51,7 @@ class Products extends Component {
                        onClick={() => this.props.addToCart(product)}
                        className="button primary"
                      >
+                          Add To Cart
+                      </button>
+                    </div>
+                  </div>
