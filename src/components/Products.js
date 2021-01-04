796be0b945ead06103e831926016e175f813ddb6
robertmoore40 <robertcmoore92@gmail.com>
2021-01-04 14:16:40 -0500

    added zoom and connect

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,3 +3,6 @@ import formatCurrency from "../util";
 
 import Fade from "react-reveal/Fade";
 import Modal from "react-modal";
+
+import Zoom from "react-reveal/Zoom";
+import { connect } from "react-redux";
