a4706032991afa115fd0c85ee37223e367155daa
robertmoore40 <robertcmoore92@gmail.com>
2021-01-06 15:35:17 -0500

    added cart reducer

--- src/reducers/cartReducers.js
+++ src/reducers/cartReducers.js
@@ -1,1 +1,5 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../types";
+
+export const cartReducer = (
+    state = { cartItems: JSON.parse(localStorage.getItem("cartItems") || "[]") },
+    action
