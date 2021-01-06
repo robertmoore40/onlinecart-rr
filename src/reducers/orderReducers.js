afdaad5d2e5f99cd5a50bd007cc041e9176d68ac
robertmoore40 <robertcmoore92@gmail.com>
2021-01-06 15:38:01 -0500

    added create orer

--- src/reducers/orderReducers.js
+++ src/reducers/orderReducers.js
@@ -2,3 +2,5 @@ import { CREATE_ORDER, CLEAR_ORDER, FETCH_ORDERS } from "../types";
 
 const orderReducer = (state = {}, action) => {
     switch (action.type) {
+        case CREATE_ORDER:
+            return { order: action.payload };
