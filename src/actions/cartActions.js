0362b4983cfb62c6d0da741fe9d707bd2a638cb5
robertmoore40 <robertcmoore92@gmail.com>
2021-01-07 08:43:22 -0500

    added exists

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -2,3 +2,5 @@ import { ADD_TO_CART, REMOVE_FROM_CART } from "../types";
 
 export const addToCart = (product) => (dispatch, getState) => {
     const cartItems = getState().cart.cartItems.slice();
+    let alreadyExists = false;
+    cartItems.forEach((x) => {
