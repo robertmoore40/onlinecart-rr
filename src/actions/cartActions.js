6977a7f2e5204406dd0eea0bdcd32b0bdf1c7aab
robertmoore40 <robertcmoore92@gmail.com>
2021-01-07 08:42:08 -0500

    added const product types

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,1 +1,4 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../types";
+
+export const addToCart = (product) => (dispatch, getState) => {
+    const cartItems = getState().cart.cartItems.slice();
