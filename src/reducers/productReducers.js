c7d91f9f6c20808a419b9b2bb16094f320bc80d6
robertmoore40 <robertcmoore92@gmail.com>
2021-01-07 08:36:26 -0500

    added reducer

--- src/reducers/productReducers.js
+++ src/reducers/productReducers.js
@@ -1,3 +1,8 @@
 import {
     FETCH_PRODUCTS,
     FILTER_PRODUCTS_BY_SIZE,
+    ORDER_PRODUCTS_BY_PRICE,
+} from "../types";
+
+export const productsReducer = (state = {}, action) => {
+    switch (action.type) {
