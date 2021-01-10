bd21e8ab51c6a07f9d4a7b24d57a5ae637f07a34
robertmoore40 <robertcmoore92@gmail.com>
2021-01-10 17:26:31 -0500

    added json app

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,3 +1,7 @@
 import { CREATE_ORDER, CLEAR_CART, CLEAR_ORDER, FETCH_ORDERS } from "../types";
 export const createOrder = (order) => (dispatch) => {
     fetch("/api/orders", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
