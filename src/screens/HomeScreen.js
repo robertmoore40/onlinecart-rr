e4fcc8a0c588f14bc46f6aebc3ee97830c67ed43
robertmoore40 <robertcmoore92@gmail.com>
2021-01-12 19:50:58 -0500

    added home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,3 +2,6 @@ import React, { Component } from "react";
 import Filter from "../components/Filter";
 import Products from "../components/Products";
 import Cart from "../components/Cart";
+
+export default class HomeScreen extends Component {
+    render() {
