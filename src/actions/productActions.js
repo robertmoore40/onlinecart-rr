a434a5a7b11a6e9a22e4766cfa4760c95c8d2c62
robertmoore40 <robertcmoore92@gmail.com>
2021-01-14 15:00:50 -0500

    added const data

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -3,3 +3,7 @@ import { FILTER_PRODUCTS_BY_SIZE, ORDER_PRODUCTS_BY_PRICE } from "../types";
 
 export const fetchProducts = () => async (dispatch) => {
   const res = await fetch("/api/products");
+
+  const data = await res.json();
+  console.log(data);
+  dispatch({
