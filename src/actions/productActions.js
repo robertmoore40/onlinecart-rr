20bb24748f7f7cc9f09b73e5e144dbe42ee3bbd7
robertmoore40 <robertcmoore92@gmail.com>
2021-01-14 15:00:37 -0500

    added fetch and res

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,2 +1,5 @@
 import { FETCH_PRODUCTS } from "../types";
 import { FILTER_PRODUCTS_BY_SIZE, ORDER_PRODUCTS_BY_PRICE } from "../types";
+
+export const fetchProducts = () => async (dispatch) => {
+  const res = await fetch("/api/products");
