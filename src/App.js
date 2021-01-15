73912abc08ac94223e36b378cbe590074f7e2bc0
robertmoore40 <robertcmoore92@gmail.com>
2021-01-15 08:16:35 -0500

    added screen imports

--- src/App.js
+++ src/App.js
@@ -3,3 +3,6 @@ import store from "./store";
 
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+
+import HomeScreen from "./screens/HomeScreen";
+import AdminScreen from "./screens/AdminScreen";
