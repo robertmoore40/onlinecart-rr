757eb3a32a22c21423ae91d9b86259f17e8515f3
robertmoore40 <robertcmoore92@gmail.com>
2021-01-15 08:27:18 -0500

    added import reducers

--- src/store.js
+++ src/store.js
@@ -1,2 +1,5 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
+
+import { productsReducer } from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
